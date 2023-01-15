d2ccb4799273a2b62399277e839a07b9d3d55c4e
Keunes <11229646+keunes@users.noreply.github.com>
2023-01-15 23:31:18 +0100

    Update guids.sh

--- .github/workflows/guids.sh
+++ .github/workflows/guids.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-search_dir=/the/path/to/base/dir/
-for post in _blog/*; do
-    if ! grep -q "guid:" "$post"; then
-        echo "Blog post $post does not specify a GUID!" >&2
-        exit 1
-    fi
+for dir in _blog _events; do
+    for file in "$dir"/*; do
+        if [ "$file" == "$dir/0_A meeting template.md" ]; then
+            continue
+        fi
+        if ! grep -qE "^(guid|uid):[ ?][^[:space:]]{5,}" "$file"; then
+            if [ "$dir" == "_blog" ]; then
+                type="blog post"
+            elif [ "$dir" == "_events" ]; then
+                type="event"
+            fi
+            echo "The $type \"${file#$dir/}\" does not specify a (G)UID!" >&2
+            exit 1
+        fi
+    done
 done
 
-duplicates=$(cat _blog/* | grep "guid:" | sort | uniq -D)
-numDuplicates=$(cat _blog/* | grep "guid:" | sort | uniq -D | wc -l)
-if [[ $numDuplicates != 0 ]]; then
-    echo "Blog post GUIDs are not unique!" >&2
-    echo "$duplicates"
-    exit 1
-fi
+check_duplicates () {
+    local folder=$1
+    local type=$2
+    local pattern=$3
+    duplicates=$(cat "$folder"/* | grep "$pattern" | sort | uniq -D)
+    numDuplicates=$(cat "$folder"/* | grep "$pattern" | sort | uniq -D | wc -l)
+    if [[ $numDuplicates != 0 ]]; then
+        pattern=${pattern%?}
+        echo "The ${type}' ${pattern^^}s are not unique!" >&2
+        echo "Duplicates found in these files:"
+        grep -l -f <(echo "$duplicates") "$folder"/*
+        exit 1
+    fi
+}
 
+check_duplicates _blog "blog posts" "guid:"
+check_duplicates _events "events" "uid:"
+
+echo "The Event & Blog post (G)UIDs are fine!"
